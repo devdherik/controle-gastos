d982e65ae6afd83abe5c1a13b06728bbf0f64935
Dherik Domingos <163038876+devdherik@users.noreply.github.com>
2026-07-07 08:30:51 -0300

    testando a primeira rquisição
    criei o metodo para consultar as pessoas no banco de dados

--- backend/Controllers/PessoasController.cs
+++ backend/Controllers/PessoasController.cs
@@ -11,11 +11,25 @@ namespace backend.Controllers;
 // controllerbase é uma classe pronta do asp.net, e aqui, estamos dizendo que pessoacontroller é um tipo especial de controller base. ':' indica herança
 public class PessoasController : ControllerBase
 {
+    //campo
     private readonly AppDbContext _context;
 
+    //construtor
     // injeção de dependencia
     public PessoasController(AppDbContext context)
     {
         _context = context;
     }
+
+
+    //método
+    [HttpGet]
+    public async Task<ActionResult<List<Pessoa>>> ListarPessoas()
+    {
+        var pessoas = await _context.Pessoas.ToListAsync();
+
+        return Ok(pessoas);
+    }
+
 }
+
