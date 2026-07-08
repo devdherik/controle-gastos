bb7b4f22f92b9e2888eb44f1ff273edf3f7a1227
Dherik Domingos <163038876+devdherik@users.noreply.github.com>
2026-07-08 20:15:03 -0300

    criei os arquivos do react com o vite e configurei o back do c# com o CORS
    Add a new frontend scaffold (Vite + React + TypeScript) under frontend: package.json, package-lock, vite.config, tsconfigs, ESLint config, index.html, App.tsx, main.tsx, CSS, assets and public icons/favicon. Also update backend/Program.cs to register a CORS policy "PermitirFrontend" (allowing http://localhost:5173) and enable it with UseCors so the new frontend can communicate with the backend.

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,6 +1,7 @@
 // permite usar a classe AppDbContext e as ferramentas do entity framework
 using backend.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 
 // cria um objeto chamado 'builder' que configura a aplicação web antes que ela seja construida e executada
@@ -22,6 +23,17 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(
     builder.Configuration.GetConnectionString("DefaultConnection")));
 
+//permite a comunicação entre o front e o back. permite que o front faça requisições para o back. chama-se CORS (cross origin resourse sharing)
+builder.Services.AddCors(Options =>
+{
+        Options.AddPolicy("PermitirFrontend", policy =>
+        {
+           policy.WithOrigins("http:localhost:5173")
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+});
+
 var app = builder.Build();
 
 
@@ -38,6 +50,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors("PermitirFrontend");
 
 app.MapControllers();
 app.Run();
